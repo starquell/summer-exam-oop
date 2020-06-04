719ef9890015cafbf29ff220ce0d3dabc6220845
YanaSkyrda <yana.skirda101010@gmail.com>
2020-06-05 02:44:35 +0300

    Template tests for trees

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,24 +4,48 @@
 #include "../include/Trees/Splay.hpp"
 #include "../include/Trees/AVL.hpp"
 #include "../include/LinkedLists/DoubleLinked.hpp"
-//#include "../include/LinkedLists/Cyclic.hpp"
 
 using namespace exam::tree;
 
-TEST_CASE("Red Black Tree") {
+SCENARIO_TEMPLATE("Red-Black and Splay Tree", Tree,
+        SplayTree<int, std::less<>>, RedBlackTree<int, std::less<>>, AVLTree<int, std::less<>>) {
+    static constexpr std::array elems = {1, 4, 103, 2, 24};
 
-    std::array elems = {1, 4, 103, 2, 24};
+    SUBCASE("Constructor from iterators test") {
+        auto tree = Tree(elems.begin(), elems.end());
+
+        auto it = tree.begin();
+
+        REQUIRE(*it == 1);
+        REQUIRE(*(++it) == 2);
+        REQUIRE(*(++it) == 4);
+        REQUIRE(*(++it) == 24);
+        REQUIRE(*(++it) == 103);
+    }
 
     SUBCASE("Constructor's from iterators test") {
-        auto tree = RedBlackTree<int, std::less<>> (elems.begin(), elems.end());
+        auto tree = Tree({elems[0], elems[1], elems[2], elems[3], elems[4]});
 
-        REQUIRE(*tree.begin() == 1);
-        REQUIRE(*(tree.begin() + 1) == 2);
-        REQUIRE(*(tree.begin() + 2) == 4);
-        REQUIRE(*(tree.begin() + 3) == 24);
-        REQUIRE(*(tree.begin() + 4) == 103);
+        auto it = tree.begin();
+
+        REQUIRE(*it == 1);
+        REQUIRE(*(++it) == 2);
+        REQUIRE(*(++it) == 4);
+        REQUIRE(*(++it) == 24);
+        REQUIRE(*(++it) == 103);
     }
-    auto tree = RedBlackTree<int, std::less<>> (elems.begin(), elems.end());
+
+    auto tree = Tree(elems.begin(), elems.end());
+
+    SUBCASE ("Deep copy") {
+        auto copied = Tree(tree);
+
+        REQUIRE(*copied.begin() == 1);
+        REQUIRE(*(copied.begin() + 1) == 2);
+        REQUIRE(*(copied.begin() + 2) == 4);
+        REQUIRE(*(copied.begin() + 3) == 24);
+        REQUIRE(*(copied.begin() + 4) == 103);
+    }
 
     SUBCASE("Insert") {
         tree.insert(100);
@@ -35,123 +59,8 @@ TEST_CASE("Red Black Tree") {
         REQUIRE_FALSE(tree.search(elems[0]));
     }
 
-    SUBCASE("Multi insertion and deletion") {
-        tree.insert(15);
-        tree.insert(203);
-        tree.insert(2);
-        tree.erase(4);
-        tree.erase(15);
-        tree.erase(45);
-        tree.insert(18);
-        tree.erase(2);
-        tree.insert(46);
-
-        REQUIRE(tree.size() == 6);
-        REQUIRE(*tree.begin() == 1);
-        REQUIRE(*(tree.begin() + 1) == 18);
-        REQUIRE(*(tree.begin() + 2) == 24);
-        REQUIRE(*(tree.begin() + 3) == 46);
-        REQUIRE(*(tree.begin() + 4) == 103);
-        REQUIRE(*(tree.begin() + 5) == 203);
-    }
-}
-
-TEST_CASE("Splay Tree") {
-
-    std::array elems = {1, 4, 103, 2, 24};
-
-    SUBCASE("Constructor's from iterators test") {
-        auto tree = SplayTree<int, std::less<>> (elems.begin(), elems.end());
-
-        REQUIRE(*tree.begin() == 1);
-        REQUIRE(*(tree.begin() + 1) == 2);
-        REQUIRE(*(tree.begin() + 2) == 4);
-        REQUIRE(*(tree.begin() + 3) == 24);
-        REQUIRE(*(tree.begin() + 4) == 103);
-    }
-    auto tree = SplayTree<int, std::less<>> (elems.begin(), elems.end());
-
-    SUBCASE("Insert") {
-        tree.insert(100);
-        REQUIRE(tree.search(100));
-    }
-
-    SUBCASE("Erase") {
-        REQUIRE(tree.search(elems[0]));
-        tree.erase(elems[0]);
-
-        REQUIRE_FALSE(tree.search(elems[0]));
-    }
-
-    SUBCASE("Multi insertion and deletion") {
-        tree.insert(15);
-        tree.insert(203);
-        tree.insert(2);
-        tree.erase(4);
-        tree.erase(15);
-        tree.erase(45);
-        tree.insert(18);
-        tree.erase(2);
-        tree.insert(46);
-
-        REQUIRE(tree.size() == 6);
-        REQUIRE(*tree.begin() == 1);
-        REQUIRE(*(tree.begin() + 1) == 18);
-        REQUIRE(*(tree.begin() + 2) == 24);
-        REQUIRE(*(tree.begin() + 3) == 46);
-        REQUIRE(*(tree.begin() + 4) == 103);
-        REQUIRE(*(tree.begin() + 5) == 203);
-    }
-}
-
-TEST_CASE("AVL Tree") {
-    std::array elems = {1, 4, 103, 2, 24};
-
-    SUBCASE("Constructor's from iterators test") {
-        auto tree = AVLTree<int, std::less<>> (elems.begin(), elems.end());
-
-        REQUIRE(*tree.begin() == 1);
-        REQUIRE(*(tree.begin() + 1) == 2);
-        REQUIRE(*(tree.begin() + 2) == 4);
-        REQUIRE(*(tree.begin() + 3) == 24);
-        REQUIRE(*(tree.begin() + 4) == 103);
-    }
-    auto tree = AVLTree<int, std::less<>> (elems.begin(), elems.end());
-
-    SUBCASE("Insert") {
-        tree.insert(100);
-        REQUIRE(tree.search(100));
-    }
-
-    SUBCASE("Erase") {
-        REQUIRE(tree.search(elems[0]));
-        tree.erase(elems[0]);
-        REQUIRE_FALSE(tree.search(elems[0]));
-
-        REQUIRE(*tree.begin() == 2);
-        REQUIRE(*(tree.begin() + 1) == 4);
-        REQUIRE(*(tree.begin() + 2) == 24);
-        REQUIRE(*(tree.begin() + 3) == 103);
-    }
-
-    SUBCASE("Multi insertion and deletion") {
-        tree.insert(15);
-        tree.insert(203);
-        tree.insert(2);
-        tree.erase(4);
-        tree.erase(15);
-        tree.erase(45);
-        tree.insert(18);
-        tree.erase(2);
-        tree.insert(46);
-
-        REQUIRE(tree.size() == 6);
-        REQUIRE(*tree.begin() == 1);
-        REQUIRE(*(tree.begin() + 1) == 18);
-        REQUIRE(*(tree.begin() + 2) == 24);
-        REQUIRE(*(tree.begin() + 3) == 46);
-        REQUIRE(*(tree.begin() + 4) == 103);
-        REQUIRE(*(tree.begin() + 5) == 203);
+    SUBCASE("Size") {
+        REQUIRE(tree.size() == std::distance(elems.begin(), elems.end()));
     }
 }
 
